afa98687f7fd1ffcef33e58386bf38cc5889ed5f
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-21 21:39:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,7 @@ def get_japanese_emoticon(file_path = "./lib/emoticons.yml", english_emoticon)
   else
     "Sorry, that emoticon was not found"
 end
+end
 
 def get_english_meaning
   # code goes here
